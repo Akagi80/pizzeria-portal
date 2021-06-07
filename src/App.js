d6947bcfae1f8d74ecf8a264fe22270e28a7ce7d
Sebastian Sikora <ssikora80@gmail.com>
2021-06-07 18:25:23 +0200

    Add components views

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import MainLayout from '../src/components/layout/MainLayout/MainLayout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Now! Right now!
-        </a>
-      </header>
-    </div>
+    <MainLayout>
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React Now! Right now!
+          </a>
+        </header>
+      </div>
+    </MainLayout>
   );
 }
 
