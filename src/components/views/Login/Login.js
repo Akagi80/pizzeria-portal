62b6f2d8fbb9b53d16918f713b7ad7c31b599430
Sebastian Sikora <ssikora80@gmail.com>
2021-06-10 19:47:41 +0200

    Add Login view

--- src/components/views/Login/Login.js
+++ src/components/views/Login/Login.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import styles from './Login.module.scss';
+import TextField from '@material-ui/core/TextField';
+import {Link} from 'react-router-dom';
+import Button from '@material-ui/core/Button';
 
 const Login = () => (
   <div className={styles.component}>
-    <h2>Login view</h2>
+    <div className={styles.log}>
+      <TextField
+        className={styles.input}
+        required
+        id="outlined-required"
+        label="Login"
+        variant="outlined"
+      />
+      <TextField
+        className={styles.input}
+        required
+        id="outlined-password-input"
+        label="Password"
+        type="password"
+        variant="outlined"
+      />
+      <div className={styles.button}>
+        <Button variant="contained" color="primary" component={Link} to={`${process.env.PUBLIC_URL}/`} activeClassName='active'>LOGIN</Button>
+      </div>
+    </div>
   </div>
 );
 
