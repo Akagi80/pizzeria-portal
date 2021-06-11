3da52ae7be206f28801a9802c197dafb07cd1c64
Sebastian Sikora <ssikora80@gmail.com>
2021-06-11 19:25:20 +0200

    Add Dashboard view

--- src/components/views/Dashboard/Dashboard.js
+++ src/components/views/Dashboard/Dashboard.js
@@ -1,5 +1,83 @@
 import React from 'react';
 import styles from './Dashboard.module.scss';
+import Container from '@material-ui/core/Container';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const demoContent = [
+  {id: 'Local', status: '56'},
+  {id: 'Delivery', status: '97'},
+];
+
+const demoContent2 = [
+  {id: 'Reservations', status: '85'},
+  {id: 'Events', status: '12'},
+];
+
+const Dashboard = () => (
+  <div className={styles.component}>
+    <Container maxWidth = 'lg'>
+      <h2 className={styles.title}>Daily statistics</h2>
+      <Paper className={styles.component}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Type</TableCell>
+              <TableCell>Number</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {demoContent.map(row => (
+              <TableRow key={row.id}>
+                <TableCell component="th" scope="row">
+                  {row.id}
+                </TableCell>
+                <TableCell>
+                  {row.status}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Paper>
+    </Container>
+    <Container maxWidth = 'lg'>
+      <h2 className={styles.title}>Reservations / events</h2>
+      <Paper className={styles.component}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Type</TableCell>
+              <TableCell>Number</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {demoContent2.map(row => (
+              <TableRow key={row.id}>
+                <TableCell component="th" scope="row">
+                  {row.id}
+                </TableCell>
+                <TableCell>
+                  {row.status}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Paper>
+    </Container>
+  </div>
+);
+
+export default Dashboard;
+
+/*
+import React from 'react';
+import styles from './Dashboard.module.scss';
 
 const Dashboard = () => (
   <div className={styles.component}>
@@ -8,3 +86,4 @@ const Dashboard = () => (
 );
 
 export default Dashboard;
+*/
